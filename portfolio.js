b3c672e5c83db03bdcc4a20421a9aec50777af77
AnujKukunarapu <anujkukunarapu12@gmail.com>
2023-03-14 19:08:42 +0530

    portfolio update

--- portfolio.js
+++ portfolio.js
@@ -13,3 +13,19 @@ btns.forEach((btn) => {
     selectedGroup.classList.add("show");
   });
 });
+
+const btns1 = document.querySelectorAll(".button1");
+const groups1 = document.querySelectorAll(".group1");
+
+btns1.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const currentBtn = document.querySelector(".button1.active");
+    currentBtn.classList.remove("active");
+    btn.classList.add("active");
+
+    const currentGroup = document.querySelector(".group1.show");
+    currentGroup.classList.remove("show");
+    const selectedGroup = document.querySelector(`.group1.${btn.classList[1]}`);
+    selectedGroup.classList.add("show");
+  });
+});
